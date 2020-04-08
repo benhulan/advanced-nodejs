9301824bafe2a24050160f4a42423584eaf07077
Ben on Ubuntu <ben@firmspace.com>
2020-04-08 13:56:59 -0500

    Has basic chat server

--- net.js
+++ net.js
@@ -1,23 +1,28 @@
-process.stdout.write('\u001B[2J\u001B[0;0f');
-
 const server = require('net').createServer();
+let counter = 0;
+let sockets = {};
 
 server.on('connection', socket => {
+	socket.id = counter++;
+	sockets[socket.id] = socket;
+
 	console.log('Client connected');
 	socket.write('Welcome new client!\n');
 
 	socket.on('data', data => {
-		console.log('data is:', data); // buffer
-		socket.write('data is: ');
-		socket.write(data);
+		Object.entries(sockets).forEach(([socketId, clientSocket]) => {
+			clientSocket.write(`${socketId}: `);
+			clientSocket.write(data);
+		})
 	});
 
 	socket.setEncoding('utf8'); // default for socket.write, but not console.log
 
 	socket.on('end', () => {
+		delete sockets[socket.id];
 		console.log('Client disconnected');
 	});
-	
+
 });
 
 server.listen(8000, () => console.log('Server bound'));
