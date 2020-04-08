7d0af54d7b108da27aa48c71af95bac2d27f90c2
Ben on Ubuntu <ben@firmspace.com>
2020-04-08 14:12:23 -0500

    Improves simple chat app

--- net.js
+++ net.js
@@ -2,21 +2,30 @@ const server = require('net').createServer();
 let counter = 0;
 let sockets = {};
 
+function timestamp() {
+	const now = new Date();
+	return `${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}`;
+}
+
 server.on('connection', socket => {
 	socket.id = counter++;
-	sockets[socket.id] = socket;
 
 	console.log('Client connected');
-	socket.write('Welcome new client!\n');
+	socket.write('Please enter your name: \n');
 
 	socket.on('data', data => {
-		Object.entries(sockets).forEach(([socketId, clientSocket]) => {
-			clientSocket.write(`${socketId}: `);
+		if (!sockets[socket.id]) {
+			socket.name = data.toString().trim();
+			socket.write(`Welcome, ${socket.name}!\n`);
+			sockets[socket.id] = socket;
+			return;
+		}
+		Object.entries(sockets).forEach(([key, clientSocket]) => {
+			if (socket.id == key) return;
+			clientSocket.write(`${socket.name} ${timestamp()}: `);
 			clientSocket.write(data);
 		})
 	});
-
-	socket.setEncoding('utf8'); // default for socket.write, but not console.log
 
 	socket.on('end', () => {
 		delete sockets[socket.id];
